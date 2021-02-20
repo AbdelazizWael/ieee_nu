b0b724f10fa7c0e491a5fe2a1692ed12fc7fbb57
Abdelaziz Wael <abdelaziz.w.f@gmail.com>
2021-02-20 17:40:10 +0200

    release 0.0.1

--- ieee_nu/urls.py
+++ ieee_nu/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/store/', include('store.urls')),
-    path('.*', TemplateView.as_view(template_name='index.html'), name='home'),
+    re_path('.*', TemplateView.as_view(template_name='index.html'), name='home'),
     path('login/', TemplateView.as_view(template_name='index.html'), name='login')
 ]
 
