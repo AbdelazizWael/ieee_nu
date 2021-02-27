9347ba2b07edbaa6fe744663caa5c006bc560b55
Abdelaziz Wael <abdelaziz.w.f@gmail.com>
2021-02-27 08:24:34 +0200

    test release

--- ieee_nu/urls.py
+++ ieee_nu/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/store/', include('store.urls')),
+    path('login/', TemplateView.as_view(template_name='index.html'), name='login'),
     re_path('.*', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('login/', TemplateView.as_view(template_name='index.html'), name='login')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
