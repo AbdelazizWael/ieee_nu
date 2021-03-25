83072524d48fa5a3ef33488d31d05cafa80480f9
Omar <o.abuelfetohahmed@nu.edu.eg>
2021-03-25 18:30:39 +0200

    class fixed

--- frontend/src/layout/components/Footer.js
+++ frontend/src/layout/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = (props) => {
     <footer style={{backgroundColor: '#495057'}} className="footer row">
         <div className="container">
         <div className="p-4 row col-12">
-                    <div className="col-lg-3 col-md-3 col-sm-6">
+                    <div className="col-lg-3 col-md-3 col-sm-6  col-6">
                         <h3>Links</h3>
                         <ul className="list-unstyled">
                             <li><a href="/home">Home</a></li>
@@ -28,7 +28,7 @@ const Footer = (props) => {
                             <i className="fas fa-phone-alt"></i> +02 1234 567 897 <br/>
                             <i className="fas fa-fax"></i> +02 1234 567 897</p>
                     </div>
-                    <div className="col-lg-5 col-md-5 col-sm-12 mb-4 col-6 mb-md-0">
+                    <div className="col-lg-5 col-md-5 col-sm-12 mb-4 mb-md-0">
                         <iframe title="Geolocation" class="mt-2" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAyzbtDMm8MS1VLeeEeA_4MVpYAxr0l8Oc&amp;q=30.0118469,30.9857909&amp;zoom=18&amp;maptype=satellite" frameborder="0" width="100%" height="100%"></iframe>
                     </div>
                     <div className="col-12 text-center mt-3">
