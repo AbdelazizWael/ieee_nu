291d65f066c5c83bbe0de77e649b0f4604ac41a8
Omar <o.abuelfetohahmed@nu.edu.eg>
2021-04-03 14:03:58 +0200

    Products card again!

--- frontend/src/layout/components/ProductCard.js
+++ frontend/src/layout/components/ProductCard.js
@@ -6,37 +6,35 @@ import '../../css/master.css'
 const ProductCard = (props) => {
     return (
         <div key={props.product.id} className="col-lg-3 col-md-4 col-sm-6 col-6 mt-4" onClick={()=> props.modal(props.product)}>
-            <div>
-                <div className="card">
-                    <div>
-                        
-                        <div class="embed-responsive embed-responsive-4by3">
-                            <img className="card-img-top embed-responsive-item" src={props.product.image} alt={props.product.name} />
+                <div>
+                    <div className="card">
+                        <div>
+                            <div class="embed-responsive embed-responsive-4by3">
+                                <img className="card-img-top embed-responsive-item" src={props.product.image} alt={props.product.name} />
+                            </div>
+                            {
+                                 props.sale ?
+                                    <div className="discount"> 
+                                        <p>{props.sale}</p>
+                                    </div> : <></>
+                            }
                         </div>
-                        {
-                             props.sale ?
-                                <div className="discount"> 
-                                    <p>{props.sale}</p>
-                                </div> : <p></p>
-                        }
-                        
+                        <div className="card-body">
+                            <a href="javascript:void(0)" onClick={()=> props.modal(props.product)}> <h5 className="card-title">{props.product.name}</h5></a>
+                            <h5 className="" style={{ color: 'green' }}>Price: {props.product.price} LE</h5>
+                            <Strap.CardSubtitle tag="p" className="text-muted my-1 categories">
+                                Categories: <br/> {props.product.categories.join(", ")}
+                            </Strap.CardSubtitle>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        <a href="javascript:void(0)" onClick={()=> props.modal(props.product)}> <h5 style={{minHeight: '48px'}} className="card-title">{props.product.name}</h5></a>
-                        <Strap.CardSubtitle tag="p" className="text-muted my-1" style={{fontSize: '1.23rem', marginBottom: '0.3rem '}}>
-                            Categories: {props.product.categories.join(", ")}
-                        </Strap.CardSubtitle>
-                        <h5 className="" style={{ color: 'green' }}>Price: {props.product.price} LE</h5>
-                    </div>
+                    {
+                        props.isAuthenticated
+                            ?
+                            <button className="card-btn" onClick={props.add(props.product)}>Add To Cart <i className="fas fa-shopping-cart"></i></button>
+                            :
+                            <button className="card-btn" disabled>Add To Cart <i className="fas fa-shopping-cart" onClick={props.add(props.product)}></i></button>
+                    }
                 </div>
-                {
-                    props.isAuthenticated
-                        ?
-                        <button className="card-btn" onClick={props.add(props.product)}>Add To Cart <i className="fas fa-shopping-cart"></i></button>
-                        :
-                        <button className="card-btn" disabled>Add To Cart <i className="fas fa-shopping-cart" onClick={props.add(props.product)}></i></button>
-                }
-            </div>
         </div>
     );
 }
